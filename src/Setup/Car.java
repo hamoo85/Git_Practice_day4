9ba856863a2c415db09a0d5c6d786ab306da22f4
mohammed <hamoo85@gmail.com>
2021-10-03 16:20:08 -0400

    updating master to origin

--- src/Setup/Car.java
+++ src/Setup/Car.java
@@ -1,18 +1,18 @@
 package Setup;
 
 public class Car {
-<<<<<<< HEAD
+
     public Car(){
         System.out.println("new car");
     }
     public Car(int a){
 
     }
-=======
+
     // this is our car from james branch
     // more text
 
 
 
->>>>>>> james
+
 }
