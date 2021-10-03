b6f8d31ac013db2454d5822bec5309988f074089
mohammed <hamoo85@gmail.com>
2021-10-03 12:46:25 -0400

    updating third file

--- src/Setup/Third.java
+++ src/Setup/Third.java
@@ -3,5 +3,9 @@ package Setup;
 public class Third {
     public static void main(String[] args) {
         System.out.println("coding java is fun");
+
+    }
+    public void m(){
+        
     }
 }
