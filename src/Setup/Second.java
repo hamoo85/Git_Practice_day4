a96228001dccf5a7783aba08899049198e9d0529
mohammed <hamoo85@gmail.com>
2021-10-03 12:04:54 -0400

    updating second file again

--- src/Setup/Second.java
+++ src/Setup/Second.java
@@ -2,9 +2,14 @@ package Setup;
 
 public class Second {
     public void mohamed(){
-
+        System.out.println("first method");
     }
     public void mohamed(int a){
-        
+        System.out.println("second method");
     }
+    public void mohamed(String str){
+        System.out.println("third method");
+    }
+
+
 }
